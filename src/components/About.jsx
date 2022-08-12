a237fad459649fc28be5c5286258a7d7ae8fc5cc
nahidfarazi <nahidfarazi19@gmail.com>
2022-08-13 00:25:27 +0600

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800
+    to-black text-white ">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col
+        justify-center w-full h-full">
+            <div className="pb-8">
+                <p className="text-4xl  font-bold inline border-b-4
+             border-gray-500">About</p>
+            </div>
+             <p className="text-xl mt-14">
+                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit doloribus assumenda, maxime dolores nemo sed odit! Nobis, voluptate. Cumque nihil eos excepturi nemo architecto! Maxime, et unde! Repellat debitis, iste dolore saepe consequatur ratione ipsum quaerat veniam sapiente dolores eius sit accusantium in at, perspiciatis repellendus non architecto eos possimus.   
+             </p>
+
+             <br />
+             <p className="text-xl mt-3">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores dicta est fugit. Deserunt iure accusamus debitis illum placeat. Voluptatibus, magni? Odit rem fuga facere pariatur quisquam saepe sapiente qui, voluptatem quis deleniti tempora voluptate ab velit possimus dolorum nemo? Impedit fuga nihil ab ratione quasi repellat rem alias voluptatibus molestias.
+             </p>
+        </div>
+    </div>
+  )
+}
+
+export default About;
---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {FaGithub,FaLinkedin} from 'react-icons/fa';
+import {HiOutlineMail} from 'react-icons/hi';
+import {BsFillPersonLinesFill} from 'react-icons/bs';
+
+const SocialLinks = () => {
+    const linkS =[
+        {
+            id:1,
+            child:(
+               <>
+                 <span className='ml-2'>Linkedin</span> <FaLinkedin size={25} />
+               </>
+            ),
+            href:"https://www.linkedin.com/in/nahid-farazi-92909a214/",
+            style:"rounded-tr-md"
+        },
+        {
+            id:2,
+            child:(
+               <>
+                 <span className='ml-2'>Github</span> <FaGithub size={25} />
+               </>
+            ),
+            href:"https://github.com/nahidfarazi",
+            
+        },
+        {
+            id:3,
+            child:(
+               <>
+                 <span className='ml-2'>Mail</span> <HiOutlineMail size={25} />
+               </>
+            ),
+            href:"mailto:nahidfarazi24@gmail.com",
+           
+        },
+        {
+            id:4,
+            child:(
+               <>
+                 <span className='ml-2'>Resume</span> <BsFillPersonLinesFill size={25} />
+               </>
+            ),
+            href:"../../public/resume.svg",
+            style:"rounded-br-md",
+            download:true,
+        }
+    ]
+  return (
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed ">
+        <ul>
+
+            {linkS.map(({id,child,href,style,download}) => (
+                
+
+                <li key={id} className={"flex justify-between items-center w-40 h-12 px-4 ml-[-100px] hover:ml-[-6px] hover:rounded-md duration-300 bg-gray-500" + " " + style}>
+                
+                <a href={href} className="flex justify-between items-center w-full
+                text-white" 
+                download={download}
+                target="_"
+                rel="noreferrer">
+
+                    {child}
+                </a>
+            </li>
+
+                
+                ))}
+
+            
+        </ul>
+    </div>
+  )
+}
+
+export default SocialLinks;
